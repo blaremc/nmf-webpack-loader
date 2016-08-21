dd261aa7332ccce799c6da43fc202104f9f490ab
Eirik Sletteberg <eiriksletteberg@gmail.com>
2016-08-21 19:03:50 +0200

    updated test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 const nmfFile = require('./lib/my-native-client-app.nmf');
 
-const expected = `data:application/json,{"program":{"x86-64":{"url":"/my-public-path/runnable-ld.c06529.so"},"x86-32":{"url":"/my-public-path/runnable-ld.b52c56.so"}},"files":{"main.nexe":{"x86-64":{"url":"/my-public-path/pi_generator_x86_64.8c6467.nexe"},"x86-32":{"url":"/my-public-path/pi_generator_x86_32.faf6b3.nexe"}},"libppapi_cpp.so":{"x86-64":{"url":"/my-public-path/libppapi_cpp.b8a2e7.so"},"x86-32":{"url":"/my-public-path/libppapi_cpp.899f61.so"}}}}`;
+const expected = `data:application/x-pnacl,{"program":{"x86-64":{"url":"/my-public-path/runnable-ld.c06529.so"},"x86-32":{"url":"/my-public-path/runnable-ld.b52c56.so"}},"files":{"main.nexe":{"x86-64":{"url":"/my-public-path/pi_generator_x86_64.8c6467.nexe"},"x86-32":{"url":"/my-public-path/pi_generator_x86_32.faf6b3.nexe"}},"libppapi_cpp.so":{"x86-64":{"url":"/my-public-path/libppapi_cpp.b8a2e7.so"},"x86-32":{"url":"/my-public-path/libppapi_cpp.899f61.so"}}}}`;
 
 if (nmfFile === expected) {
   console.log('TEST OK');
